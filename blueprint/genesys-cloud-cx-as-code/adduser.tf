85b4c33225f8790f02a6aec084f35330c8e2bff3
xihzhang <100363722+xihzhang@users.noreply.github.com>
2024-03-06 04:58:17 +0800

    Update adduser.tf

--- blueprint/genesys-cloud-cx-as-code/adduser.tf
+++ blueprint/genesys-cloud-cx-as-code/adduser.tf
@@ -1,6 +1,6 @@
 resource "genesyscloud_user" "example_user" {
-  email           = "Mark@example.com"
-  name            = "MarkTerraPOCTest"
+  email           = "Terra1@example.com"
+  name            = "TerraPOCTest"
   password        = "Initial-Password123!"
   #division_id     = genesyscloud_auth_division.home.id
   state           = "active"
@@ -21,3 +21,48 @@ resource "genesyscloud_user" "example_user" {
       type       = "MOBILE"
     }
   }
+  #routing_skills {
+  #  skill_id    = genesyscloud_routing_skill.test-skill.id
+  #  proficiency = 4.5
+  #}
+  #routing_languages {
+  #  language_id = genesyscloud_routing_language.english.id
+  #  proficiency = 4
+  #}
+  #locations {
+  #  location_id = genesyscloud_location.main-site.id
+  #  notes       = "Office 201"
+  #}
+  employer_info {
+    official_name = "Jonathon Doe"
+    employee_id   = "12345"
+    employee_type = "Full-time"
+    date_hire     = "2021-03-18"
+  }
+  routing_utilization {
+    call {
+      maximum_capacity = 1
+      include_non_acd  = true
+    }
+    callback {
+      maximum_capacity          = 2
+      include_non_acd           = false
+      interruptible_media_types = ["call", "email"]
+    }
+    chat {
+      maximum_capacity          = 3
+      include_non_acd           = false
+      interruptible_media_types = ["call"]
+    }
+    email {
+      maximum_capacity          = 2
+      include_non_acd           = false
+      interruptible_media_types = ["call", "chat"]
+    }
+    message {
+      maximum_capacity          = 4
+      include_non_acd           = false
+      interruptible_media_types = ["call", "chat"]
+    }
+  }
+}
